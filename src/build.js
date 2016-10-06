db02415e2022f0bf6589792ce34ad7e431219e6c
Jan T. Sott <jan@idleberg.com>
2016-10-06 16:29:50 +0200

    update file

--- src/build.js
+++ src/build.js
@@ -1,40 +1,36 @@
 'use strict';
 
 // Dependencies
-var EmojiData = require('emoji-data-2016');
-var CSON = require('cson');
-var fs = require('fs');
+const EmojiData = require('emoji-data-2016');
+const CSON = require('cson');
+const fs = require('fs');
 
 // Variables & Constants
-var meta = require('../package.json');
-var outputDir = "snippets";
-var emojiAll = EmojiData.all();
+const meta = require('../package.json');
+const outputDir = "snippets";
+const emojiAll = EmojiData.all();
 var snippets = {};
 
 // Main
-console.log(meta.name + ' v' + meta.version + '\nThe MIT License\n');
+console.log(`${meta.name} v${meta.version}\nThe MIT License\n`);
 
-fs.exists(outputDir, function (exists) {
-    if (!exists) {
-        console.log('\u1F4AB ./' + outputDir);
-        fs.mkdirSync(outputDir);
-    }
-    writeSnippets("css", ".source.css, .source.sass", "content: '\\\\", "';");
-    writeSnippets("html", ".text.html", "&#x");
-    writeSnippets("javascript", ".source.js", '\\\\u');
-    writeSnippets("ruby", ".source.ruby", '\\\\u{', "}");
+fs.exists(outputDir, (exists) => {
+  if (!exists) {
+    console.log(`\u1F4AB ./${outputDir}`);
+    fs.mkdirSync(outputDir);
+  }
+  writeSnippets("css", ".source.css, .source.sass", "content: '\\\\", "';");
+  writeSnippets("html", ".text.html", "&#x", "");
+  writeSnippets("javascript", ".source.js", "\\\\u", "");
+  writeSnippets("ruby", ".source.ruby", "\\\\u{", "}", "");
 });
 
 // Functions
-function writeSnippets(type, scope) {
-    var prefix = arguments.length > 2 && arguments[2] !== undefined ? arguments[2] : "";
-    var suffix = arguments.length > 3 && arguments[3] !== undefined ? arguments[3] : "";
-
+function writeSnippets(type, scope, prefix, suffix) {
     for (var i = 0; i < emojiAll.length; i++) {
-        var name = void 0,
-            unicode = void 0;
+        var emoji, json, name, output, unicode;
 
-        if (typeof emojiAll[i].name != 'undefined' && emojiAll[i].name !== null) {
+        if (typeof emojiAll[i].name != 'undefined' && emojiAll[i].name !== null ) {
             name = emojiAll[i].name.toLowerCase().replace(/\s/g, "-");
         } else {
             name = emojiAll[i].short_name;
@@ -44,20 +40,20 @@ function writeSnippets(type, scope) {
         emoji = emojiAll[i].render();
 
         snippets[emoji] = {
-            "body": '' + prefix + unicode + suffix,
-            "prefix": 'ji:' + name
+            "body": `${prefix}${unicode}${suffix}`,
+            "prefix": `ji:${name}`
         };
     }
 
-    var json = {};
+    json = {};
     json[scope] = snippets;
 
     // Object to CSON
     output = CSON.stringify(json, null, 2);
 
     // Save file
-    fs.writeFile(outputDir + '/emoji-' + type + '.cson', output, function (err) {
-        if (err) throw err;
-        console.log('\u2705 emoji-' + type + '.cson');
+    fs.writeFile(`${outputDir}/emoji-${type}.cson`, output, (err) => {
+      if (err) throw err;
+      console.log(`\u2705 emoji-${type}.cson`);
     });
 }
